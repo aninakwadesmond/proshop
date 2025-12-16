e2eeec7f0e82711e24f2081a9005d7cf007a0661
aninakwadesmond <aninakwadesmond3@gmail.com>
2025-12-16 01:29:22 +0000

    change

--- backend/utils/connect.js
+++ backend/utils/connect.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose');
 
 module.exports = async function () {
-  return mongoose
-    .connect('mongodb://localhost/proshop')
-    .then(() => {
-      console.log('Connected to the db ');
-    })
-    .catch((err) => console.log(err, err.message));
+  return (
+    mongoose
+      // .connect('mongodb://localhost/proshop')
+      .connect(
+        'mongodb+srv://aninakwahdesmond3_db_user:@mista334@cluster0.odjki9w.mongodb.net/?appName=Cluster0'
+      )
+      .then(() => {
+        console.log('Connected to the db ');
+      })
+      .catch((err) => console.log(err, err.message))
+  );
 };
 //
 // {
