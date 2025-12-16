b5d56ad617d61b77997e3c772b8b6f626c8948e0
aninakwadesmond <aninakwadesmond3@gmail.com>
2025-12-16 10:48:05 +0000

    compiler resolve

--- Shopify/vite.config.js
+++ Shopify/vite.config.js
@@ -4,21 +4,5 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-        // server: {
-        //   proxy: {
-        //     '/proshop': {
-        //       target: 'http://localhost:5000',
-        //       changeOrigin: true,
-        //       secure: false,
-        //     },
-        //   },
-        // },
-      },
-    }),
-  ],
+  plugins: [tailwindcss(), react()],
 });
