a5394fda2953af0c0ce8fee93670efbcde3e9990
aninakwadesmond <aninakwadesmond3@gmail.com>
2025-12-18 01:09:02 +0000

    data imageang22edtd

--- Shopify/src/components/Navigation.jsx
+++ Shopify/src/components/Navigation.jsx
@@ -180,7 +180,7 @@ function Navigation() {
                 </div>
               )}
             </button>
-            {userInfo && userInfo.isAdmin === 'true' && (
+            {userInfo && userInfo.isAdmin !== 'false' && (
               <div className="flex flex-col items-center justify-start relative">
                 <div
                   className="flex items-center justify-start gap-1 cursor-pointer"
