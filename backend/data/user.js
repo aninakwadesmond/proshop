6237c9beb95cdd4e9abf7696375bc2478c4c342f
aninakwadesmond <aninakwadesmond3@gmail.com>
2025-12-18 01:40:52 +0000

    data imageang22e2dtd

--- backend/data/user.js
+++ backend/data/user.js
@@ -2,11 +2,11 @@ const user = [
   {
     name: 'dessy',
     email: 'dessy33@gmail.com',
-    password: '1234',
+    password: '@mista334',
     isAdmin: true,
   },
-  { name: 'dessy2', email: 'dessy332@gmail.com', password: '1234' },
-  { name: 'dessy3', email: 'dessy333@gmail.com', password: '1234' },
+  { name: 'dessy2', email: 'dessy332@gmail.com', password: '@mista334' },
+  { name: 'dessy3', email: 'dessy333@gmail.com', password: '@mista334' },
 ];
 
 module.exports = user;
